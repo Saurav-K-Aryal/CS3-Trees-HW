2f42003fc818ad8283cf5e6c87ba7ca6c2367360
Saurav Keshari Aryal <saurav.aryal@bison.howard.edu>
2016-04-02 08:11:27 -0400

    some formatting changes

--- trees.h
+++ trees.h
@@ -125,12 +125,6 @@ class BinarySearchTree
            root = NULL;
         }
         bool isEmpty() const { return root==NULL; }
-        void print_inorder();
-        void inorder(tree_node*);
-        void print_preorder();
-        void preorder(tree_node*);
-        void print_postorder();
-        void postorder(tree_node*);
         void insert(int);
         void remove(int);
 };
