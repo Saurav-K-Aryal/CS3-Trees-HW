fb9ebe528422e975331be286e7f86f7d22d3d340
Saurav Keshari Aryal <saurav.aryal@bison.howard.edu>
2016-04-02 02:43:07 -0400

    Added code for BinarySearch Tree

--- main.cpp
+++ main.cpp
@@ -1,97 +1,121 @@
 #include "trees.h"
 #include <time.h>
+#include <algorithm>    // random_shuffle
+#include <vector>       // vector
+#include <ctime>        // time
+#include <cstdlib>      // rand, srand
 
 using namespace std;
 
 // Driver program to test above functions
 
+int myrandom (int i) { return rand()%i;}
 
 int main()
 {
-    //initializing two clock variables to measure execution time.
-    clock_t t1,t2;
+    cout << "\n-------------------------------------------------------------------\n";
 
-    
+  srand(unsigned(time(0)));
+  vector<int> myvector;
+
+  cout << "Initializing a random vector with 1 million values";
+
+  cout << endl << "Values are unique and inclue 1 and 1 million" << endl;
+  // set some values:
+  for (int i=1; i<=1000000; i++) myvector.push_back(i); // 1 2 3 4 5 6 7 8 9
+
+  // using built-in random generator:
+  random_shuffle ( myvector.begin(), myvector.end() );
+
+  // using myrandom:
+  random_shuffle ( myvector.begin(), myvector.end(), myrandom);
+
+  cout << '\n';
+ //initializing two clock variables to measure execution time.
+    clock_t t1,t2;
+   cout << "**********************************************************************\n";
+
+    /* For BinarySearch Tree */    
+
+    cout << "For a BinarySearch Tree\n\n";
+    cout << "Inserting unique values from 1 to 1,000,000 randomly\n";
+
+    // start clock
+    t1 = clock();
+
+	BinarySearchTree bst;
+
+  for (vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it) {
+    bst.insert(*it);
+}
+    t2 = clock();         // end clock
+
+    float time_diff = ((float)t2-(float)t1);
+
+    // converting the time to seconds before console printing
+    // CLOCKS_PER_SEC is a macro from the time library
+    cout << "Net time taken for insert:(in seconds) " << time_diff / CLOCKS_PER_SEC << endl;
+
+
+    // deleting values in the same order:
+
+    cout << "\nDeleting values from 1 to 1,000,000 serially in ascending order\n";
+
+    t1 = clock();
+
+for (int i = 1; i <= 1000000; i++) {
+    bst.remove(i);
+};
+    t2 = clock();         // end clock
+
+    time_diff = ((float)t2-(float)t1);
+
+    // converting the time to seconds before console printing
+    // CLOCKS_PER_SEC is a macro from the time library
+    cout << "Net time taken for delete:(in seconds) " << time_diff / CLOCKS_PER_SEC << endl;
+
+
+    cout << "\n**********************************************************************\n";
     /* For 2-3 B Tree */    
 
-    cout << "For a 2-3 B Tree\n\n";
-    cout << "Insert values from 1 to 1,000,000 serially in ascending order\n";
+    cout << "\nFor a 2-3 B Tree\n\n";
+    cout << "Inserting unique values from 1 to 1,000,000 randomly\n";
 
     // start clock
     t1 = clock();
 
     BTree Bt(3); 
     
-    for (int i = 1; i <= 1000000; i++) {
-        Bt.insert(i);
-    }
+ for (vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it) {
+    Bt.insert(*it);
+}
     t2 = clock();         // end clock
 
-    float ascending_B_tree_diff = ((float)t2-(float)t1);
+    time_diff = ((float)t2-(float)t1);
 
     // converting the time to seconds before console printing
     // CLOCKS_PER_SEC is a macro from the time library
-    cout << "net time taken for insert:(in seconds) " << ascending_B_tree_diff / CLOCKS_PER_SEC << endl;
+    cout << "Net time taken for insert:(in seconds) " << time_diff / CLOCKS_PER_SEC << endl << endl;
 
 
     // deleting values in the same order:
 
-    cout << "Delete values from 1 to 1,000,000 serially in ascending order\n";
+    cout << "\nDeleting values from 1 to 1,000,000 serially in ascending order\n";
 
     t1 = clock();
 
-    for (int i = 1; i <= 1000000; i++) {
-        Bt.remove(i);
-    }
+ for (int i = 1; i <= 1000000; i++) {
+    Bt.remove(i);
+}
     t2 = clock();         // end clock
 
-    ascending_B_tree_diff = ((float)t2-(float)t1);
+    time_diff = ((float)t2-(float)t1);
 
     // converting the time to seconds before console printing
     // CLOCKS_PER_SEC is a macro from the time library
-    cout << "net time taken for delete:(in seconds) " << ascending_B_tree_diff / CLOCKS_PER_SEC << endl;
+    cout << "net time taken for delete:(in seconds) " << time_diff / CLOCKS_PER_SEC << endl;
 
- 
-    // /* For Red Black Tree */    
-    // cout << "\n\nFor a Red Black Tree\n\n";
-    // cout << "Insert values from 1 to 1,000,000 serially in ascending order\n";   
-
-    // // start clock
-    // t1 = clock();
-
-    // RBtree RBt;
-    
-    // for (int i = 1; i <= 1000000; i++) {
-    //     RBt.insert(i);
-    // }
-    // t2 = clock();         // end clock
-
-    // ascending_B_tree_diff = ((float)t2-(float)t1);
-
-    // // converting the time to seconds before console printing
-    // // CLOCKS_PER_SEC is a macro from the time library
-    // cout << "net time taken for insert:(in seconds) " << ascending_B_tree_diff / CLOCKS_PER_SEC << endl;
-
-
-    // // deleting values in the same order:
-
-    // cout << "Delete values from 1 to 1,000,000 serially in ascending order\n";
-
-    // t1 = clock();
-
-    // for (int i = 1; i <= 1000000; i++) {
-    //     RBt.del(i);
-    // }
-    // t2 = clock();         // end clock
-
-    // ascending_B_tree_diff = ((float)t2-(float)t1);
-
-    // // converting the time to seconds before console printing
-    // // CLOCKS_PER_SEC is a macro from the time library
-    // cout << "net time taken for delete:(in seconds) " << ascending_B_tree_diff / CLOCKS_PER_SEC << endl;
-
-
-
+   cout << "**********************************************************************\n";
 
     return 0;
 }
--- trees.h
+++ trees.h
@@ -107,3 +107,31 @@ public:
     void remove(int k);
  
 };
+
+/* ClasS definition of BST. */
+
+class BinarySearchTree
+{
+    private:
+        struct tree_node
+        {
+           tree_node* left;
+           tree_node* right;
+           int data;
+        };
+        tree_node* root;
+    public:
+        BinarySearchTree()
+        {
+           root = NULL;
+        }
+        bool isEmpty() const { return root==NULL; }
+        void print_inorder();
+        void inorder(tree_node*);
+        void print_preorder();
+        void preorder(tree_node*);
+        void print_postorder();
+        void postorder(tree_node*);
+        void insert(int);
+        void remove(int);
+};
